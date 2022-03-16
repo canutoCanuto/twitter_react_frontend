a1fcd2e611b41e70fb81d4247681d1bcb70c0f4d
soriasebastian <soriagorgoroso@gmail.com>
2022-03-16 11:42:25 -0300

    Update SideBarLeft.jsx
    Cambios SidBar

--- src/components/SideBarLeft.jsx
+++ src/components/SideBarLeft.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+
 function SideBarL() {
   return (
     <>
       <a href="/home">
-        <div className="div-container p-2 rounded-pill mt-2" id="home-icon">
+        <div
+          className="div-container p-2 rounded-pill mt-2 mb-3"
+          id="home-icon"
+        >
           <div className="icon-container">
             <svg viewBox="0 0 24 24" aria-hidden="true" className="logo">
               <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
@@ -21,7 +25,7 @@ function SideBarL() {
               </g>
             </svg>
           </div>
-          <div className="centered px-3 fs-2 d-none d-md-block align-items-center">
+          <div className="centered px-3 fs-2 mb-2  d-none d-md-block ">
             <span className="div-container-text ">Home</span>
           </div>
         </div>
@@ -36,7 +40,7 @@ function SideBarL() {
               </g>
             </svg>
           </div>
-          <div className="centered px-3 fs-2 d-none d-md-block">
+          <div className="centered px-3 fs-2 mb-2 d-none d-md-block">
             <span className="div-container-text">Explore</span>
           </div>
         </div>
@@ -51,7 +55,7 @@ function SideBarL() {
               </g>
             </svg>
           </div>
-          <div className="centered px-3 fs-2 d-none d-md-block">
+          <div className="centered px-3 fs-2 mb-2 d-none d-md-block">
             <span className="div-container-text">Notifications</span>
           </div>
         </div>
@@ -66,7 +70,7 @@ function SideBarL() {
               </g>
             </svg>
           </div>
-          <div className="centered px-3 fs-2 d-none d-md-block">
+          <div className="centered px-3 fs-2 mb-2 d-none d-md-block">
             <span className="div-container-text">Messages</span>
           </div>
         </div>
@@ -80,7 +84,7 @@ function SideBarL() {
               </g>
             </svg>
           </div>
-          <div className="centered px-3 fs-2 d-none d-md-block">
+          <div className="centered px-3 fs-2 mb-2 d-none d-md-block">
             <span className="div-container-text">Bookmarks</span>
           </div>
         </div>
@@ -95,7 +99,7 @@ function SideBarL() {
               </g>
             </svg>
           </div>
-          <div className="centered px-3 fs-2 d-none d-md-block">
+          <div className="centered px-3 fs-2 mb-2 d-none d-md-block">
             <span className="div-container-text">Lists</span>
           </div>
         </div>
@@ -109,7 +113,7 @@ function SideBarL() {
               </g>
             </svg>
           </div>
-          <div className="centered px-3 fs-2 d-none d-md-block">
+          <div className="centered px-3 fs-2 mb-2 d-none d-md-block">
             <span className="div-container-text">Profile</span>
           </div>
         </div>
@@ -126,12 +130,12 @@ function SideBarL() {
               </g>
             </svg>
           </div>
-          <div className="centered px-3 fs-2 d-none d-md-block">
+          <div className="centered px-3 fs-2 mb-2 d-none d-md-block">
             <span className="div-container-text">More</span>
           </div>
         </div>
       </a>
-      <div className="centered px-3 fs-2">
+      <div className="centered px-3 fs-2 mb-2">
         <button className="btn rounded-pill btn-tweet py-2">
           <span>Tweet</span>
         </button>
