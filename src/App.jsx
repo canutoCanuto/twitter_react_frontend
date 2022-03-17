4c213bb35b16ec30c581715524cb14e035d95979
Mauro Montero <93015917+canutoCanuto@users.noreply.github.com>
2022-03-16 21:12:21 -0300

    implemento Login page, primer intento de pedir token al backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
+import Login from "./components/Login";
 import "./components/SideBarLeft.css";
 import "./components/SideBarRight.css";
 import Home from "./pages/Home";
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/user" element={<User />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,123 @@
 import React from "react";
+import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
+import axios from "axios";
 
 function Login() {
-  return <div>Login</div>;
+  const [show, setShow] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(process.env.API_URL + "/users/login", {
+        username: username,
+        password: password,
+      });
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <Button variant="primary" onClick={handleShow}>
+        Log In
+      </Button>
+      <Modal
+        id="staticBackdrop"
+        tabIndex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+        show={show}
+        onHide={handleClose}
+      >
+        <Modal.Body
+          className="modal-content bg-black text-light p-3 d-inline-block"
+          // style="border-radius: 20px"
+        >
+          <div className="d-flex justify-content-between modal-body">
+            <div>
+              <button
+                id=""
+                type="button"
+                className="btn-close btn-close-white"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="28"
+              height="28"
+              viewBox="0 0 24 24 "
+              className="logobox"
+            >
+              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+            </svg>
+            <div></div>
+          </div>
+
+          <div className="ps-4">
+            <div className="modal-body">
+              <h5 className="modal-title text-white" id="staticBackdropLabel">
+                Iniciar sesión
+              </h5>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className="form-floating mb-3">
+                <input
+                  name="username"
+                  type="text"
+                  className="form-control bg-dark text-white"
+                  id="username"
+                  placeholder="Username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <label className="text-secondary" htmlFor="floatingInput">
+                  Username
+                </label>
+              </div>
+              <div className="form-floating">
+                <input
+                  name="password"
+                  type="text"
+                  className="form-control bg-dark text-white"
+                  id="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label className="text-secondary" htmlFor="floatingPassword">
+                  Password
+                </label>
+              </div>
+
+              <div className="row g-2">
+                <div className="d-grid gap-2 py-5">
+                  <button
+                    type="submit"
+                    className="btn btn-secondary rounded-pill text-black"
+                  >
+                    Siguiente
+                  </button>
+                </div>
+              </div>
+            </form>
+
+            <div className="card-footer text-muted">
+              ¿No tienes una cuenta? <a href="/checkin">Regístrate</a>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
 }
 
 export default Login;
