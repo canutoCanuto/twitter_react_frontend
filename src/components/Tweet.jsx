d7173377e20d96fb037ece3adfebcc08d6191cef
PabloSpr <sw.pablo@gmail.com>
2022-03-18 15:55:35 -0300

    alineaciones

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -27,25 +27,26 @@ function Tweet({ tweet }) {
       });
       const upDatedTweet = response.data.upDatedTweet;
       dispatch(actions.like(upDatedTweet));
+      setLike((prev) => !like);
     } catch (error) {
       console.log(error);
     }
   };
 
   return (
-    <Row className="mx-1 tweet bg-black text-light">
-      <Col className="border-bottom">
-        <div className="d-flex pt-3 justify-content-center">
-          <div>
-            <Image
-              src={tweet.author.avatar}
-              alt="Avatar"
-              roundedCircle={true}
-              className="profile-avatar"
-            />
-          </div>
+    <Row className="mx-1 tweet bg-black text-light border-bottom">
+      <Col lg={2} className="mx-0 px-0 pt-3">
+        <Image
+          src={tweet.author.avatar}
+          alt="Avatar"
+          roundedCircle={true}
+          className="profile-avatar "
+        />{" "}
+      </Col>
+      <Col className="ps-0">
+        <div className=" pt-3 ">
           <div className="ms-3">
-            <Row className="justify-content-between text-start">
+            <Row className=" text-start d-flex justify-content-between">
               <Col md={11}>
                 <span className="fw-bold">
                   <a href="https://twitter.com">
@@ -57,7 +58,7 @@ function Tweet({ tweet }) {
                   @{tweet.author.username} · {/* {tweet.createdAt}  */}
                 </span>
               </Col>
-              <Col md={1} className="flex-row-reverse">
+              <Col md={1} className="">
                 <a href="https://twitter.com">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -95,13 +96,31 @@ function Tweet({ tweet }) {
               </Col>
               <Col>
                 <span onClick={() => submitLike()}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512"
-                    className="logo-normal"
-                  >
-                    <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z" />
-                  </svg>
+                  {!like ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 512 512"
+                      className="logo-normal"
+                    >
+                      <path d="M244 84L255.1 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 0 232.4 0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84C243.1 84 244 84.01 244 84L244 84zM255.1 163.9L210.1 117.1C188.4 96.28 157.6 86.4 127.3 91.44C81.55 99.07 48 138.7 48 185.1V190.9C48 219.1 59.71 246.1 80.34 265.3L256 429.3L431.7 265.3C452.3 246.1 464 219.1 464 190.9V185.1C464 138.7 430.4 99.07 384.7 91.44C354.4 86.4 323.6 96.28 301.9 117.1L255.1 163.9z" />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-heart-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                      />
+                    </svg>
+                  )}
+
+                  <i className="ms-1">{tweet.likes.length}</i>
                 </span>
               </Col>
               <Col>
