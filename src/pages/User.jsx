b521f4ee142d4eb2793308570267caeb98169726
maxiCaballo <caballomaxi@gmail.com>
2022-03-17 23:45:51 -0300

    avance profile

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,9 +1,37 @@
-import React from "react";
 import SideBarLeft from "../components/SideBarLeft";
 import SideBarRight from "../components/SideBarRight";
 import { Container, Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import TweetProfile from "../components/TweetProfile";
 
 function User() {
+  const tweetList = useSelector((state) => state.tweets);
+  const user = useSelector((state) => state.users[0].username);
+  const token = useSelector((state) => state.users[0].token);
+  const [tweets, setTweets] = useState([]);
+  const [postUser, setPostUser] = useState({});
+  const [joinedDate, setJoinedDate] = useState("");
+
+  const getProfileTweets = async () => {
+    try {
+      const { data } = await axios.get(
+        process.env.REACT_APP_API_URL + `/users/${user}`,
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+      const { postUser, tweets, formattedDate } = data;
+      setTweets([...tweets]);
+      setPostUser({ ...postUser });
+      setJoinedDate(formattedDate);
+      console.log(postUser, tweets);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getProfileTweets();
+  }, []);
   return (
     <Container>
       <Row>
@@ -24,37 +52,46 @@ function User() {
               </a>
             </div>
             <div className="col-11">
-              <p className="fw-bold fs-4 mb-0 text-light">firstname lastname</p>
-              <p className="text-muted mb-1">n Tweets</p>
+              <p className="fw-bold fs-4 mb-0 text-light text-start">
+                {postUser.firstname}
+              </p>
+              <p className="text-muted mb-1 text-start">
+                {tweets.length < 2 ? tweets.length + " Tweet" : "Tweets"}
+              </p>
             </div>
           </div>
-          <div className="row mx-1 bg-black text-light">
-            <div className="col-5">
+          <div className="row mx-1 bg-black text-light border-bottom ">
+            <div className="col-5 mb-5">
               <img
-                src=""
+                src={postUser.avatar}
                 alt="Avatar"
                 // style={{width: 150px; height: 150px}}
-                className="rounded-circle border border-dark"
+                className="rounded-circle border border-dark w-50 h-50"
               />
-              <p className="fw-bold fs-4 mb-0 text-light">firstnamelastname</p>
-              <p className="text-muted">@username</p>
-              <p className="text-light">description</p>
-              <p>
+              <p className="fw-bold fs-4 mb-0 text-light text-start">
+                {postUser.firstname} {postUser.lastname}
+              </p>
+              <p className="text-muted text-start">@{user}</p>
+              <p className="">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 448 512"
-                  className="logo-small"
+                  className="logo-small a"
                 >
                   <path d="M160 32V64H288V32C288 14.33 302.3 0 320 0C337.7 0 352 14.33 352 32V64H400C426.5 64 448 85.49 448 112V160H0V112C0 85.49 21.49 64 48 64H96V32C96 14.33 110.3 0 128 0C145.7 0 160 14.33 160 32zM0 192H448V464C448 490.5 426.5 512 400 512H48C21.49 512 0 490.5 0 464V192zM64 304C64 312.8 71.16 320 80 320H112C120.8 320 128 312.8 128 304V272C128 263.2 120.8 256 112 256H80C71.16 256 64 263.2 64 272V304zM192 304C192 312.8 199.2 320 208 320H240C248.8 320 256 312.8 256 304V272C256 263.2 248.8 256 240 256H208C199.2 256 192 263.2 192 272V304zM336 256C327.2 256 320 263.2 320 272V304C320 312.8 327.2 320 336 320H368C376.8 320 384 312.8 384 304V272C384 263.2 376.8 256 368 256H336zM64 432C64 440.8 71.16 448 80 448H112C120.8 448 128 440.8 128 432V400C128 391.2 120.8 384 112 384H80C71.16 384 64 391.2 64 400V432zM208 384C199.2 384 192 391.2 192 400V432C192 440.8 199.2 448 208 448H240C248.8 448 256 440.8 256 432V400C256 391.2 248.8 384 240 384H208zM320 432C320 440.8 327.2 448 336 448H368C376.8 448 384 440.8 384 432V400C384 391.2 376.8 384 368 384H336C327.2 384 320 391.2 320 400V432z" />
                 </svg>
-                <span className="text-muted"> Joined formattedDate</span>
+                <span className="text-muted"> Joined {joinedDate}</span>
               </p>
               <p className="text-light">
-                following.length <span className="text-muted">Following</span>
-                followers.length <span className="text-muted">Followers</span>
+                error
+                <span className="text-muted me-2">Following </span>
+                error
+                <span className="text-muted">Followers </span>
               </p>
             </div>
           </div>
+
+          <TweetProfile user={postUser} tweet={tweets} />
         </Col>
         <Col lg={4}>
           <SideBarRight />
