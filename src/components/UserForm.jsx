985c07ecf58999a1f83bc37df36e89676dae3153
soriasebastian <soriagorgoroso@gmail.com>
2022-03-21 12:03:38 -0300

    Register estilos
    Register estilos

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -14,6 +14,7 @@ import {
   Button,
   FloatingLabel,
   Form,
+  CloseButton,
 } from "react-bootstrap";
 
 function UserForm(props) {
@@ -78,16 +79,29 @@ function UserForm(props) {
       >
         <Container className="backmodal">
           <Row className="space d-flex bd-highlight">
-            <Col className="p-4 w-100 bd-highlight ">
-              <span className="tituloCrear">Crea tu cuenta</span>
-            </Col>
-            <Col className="p-2 flex-shrink-1 bd-highlight text-end p-4">
-              <span>
-                <svg viewBox="0 0 24 24" aria-hidden="true" className="logobox">
-                  <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
-                </svg>
-              </span>
-            </Col>
+            <Row className="justify-content-end">
+              <Col className="m-3 ">
+                <CloseButton onClick={handleClose} className="text-light ">
+                  X
+                </CloseButton>
+              </Col>
+              <Col></Col>
+              <Col></Col>
+              <Col>
+                <span className="">
+                  <svg
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                    className="logobox m-3  "
+                  >
+                    <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
+                  </svg>
+                </span>
+              </Col>
+            </Row>
+            <Row className="p-4 w-100 bd-highlight ">
+              <h1 className="tituloCrear">Crea tu cuenta</h1>
+            </Row>
           </Row>
         </Container>
         <Modal.Body className="show-grid backmodal">
@@ -95,55 +109,63 @@ function UserForm(props) {
             <Row>
               <Col xs={12} md={8} className="form-floating mb-4 labeltext">
                 <FloatingLabel
-                  className="m-3"
+                  className="m-3 text-secondary"
                   controlId="Nombre"
                   label="Nombre"
                 >
                   <Form.Control
+                    className="fondo"
                     type="text"
-                    placeholder="Nombre"
+                    placeholder="First Name"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                   />
                 </FloatingLabel>
                 <FloatingLabel
-                  className="m-3"
+                  className="m-3 text-secondary"
                   controlId="Apellido"
                   label="Apellido"
                 >
                   <Form.Control
+                    className="fondo"
                     type="text"
-                    placeholder="Apellido"
+                    placeholder="Last Name"
                     value={surname}
                     onChange={(e) => setSurname(e.target.value)}
                   />
                 </FloatingLabel>
                 <FloatingLabel
-                  className="m-3"
+                  className="m-3 text-secondary"
                   controlId="Username"
                   label="Username"
                 >
                   <Form.Control
+                    className="fondo"
                     type="text"
                     placeholder="Username"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                   />
                 </FloatingLabel>
-                <FloatingLabel className="m-3" controlId="Email" label="Email">
+                <FloatingLabel
+                  className="m-3 text-secondary"
+                  controlId="Email"
+                  label="Email"
+                >
                   <Form.Control
-                    type="email"
+                    className="fondo"
                     placeholder="Email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                   />
                 </FloatingLabel>
                 <FloatingLabel
-                  className="m-3"
+                  className="m-3 text-secondary"
                   controlId="Password"
                   label="Password"
                 >
                   <Form.Control
+                    className="fondo"
                     type="password"
                     placeholder="Password"
                     value={password}
@@ -163,11 +185,14 @@ function UserForm(props) {
             <Row>
               <Col xs={6} md={4}>
                 <FloatingLabel
-                  className=""
+                  className="text-secondary"
                   controlId="floatingSelectGrid"
                   label="Dia"
                 >
-                  <Form.Select aria-label="Floating label select example">
+                  <Form.Select
+                    className="fondo "
+                    aria-label="Floating label select example"
+                  >
                     <option>-</option>
                     <option value="1">One</option>
                     <option value="2">Two</option>
@@ -177,11 +202,14 @@ function UserForm(props) {
               </Col>
               <Col xs={6} md={4}>
                 <FloatingLabel
-                  className=""
+                  className="text-secondary"
                   controlId="floatingSelectGrid"
                   label="Mes"
                 >
-                  <Form.Select aria-label="Floating label select example">
+                  <Form.Select
+                    className="fondo"
+                    aria-label="Floating label select example"
+                  >
                     <option>-</option>
                     <option value="1">One</option>
                     <option value="2">Two</option>
@@ -191,11 +219,14 @@ function UserForm(props) {
               </Col>
               <Col xs={6} md={4}>
                 <FloatingLabel
-                  className=""
+                  className="text-secondary"
                   controlId="floatingSelectGrid"
                   label="Año"
                 >
-                  <Form.Select aria-label="Floating label select example">
+                  <Form.Select
+                    className="fondo"
+                    aria-label="Floating label select example"
+                  >
                     <option>-</option>
                     <option value="1">One</option>
                     <option value="2">Two</option>
@@ -207,7 +238,6 @@ function UserForm(props) {
           </Container>
         </Modal.Body>
         <Modal.Footer className="backmodal">
-          <Button onClick={handleClose}>Close</Button>
           <Button onClick={handleRegister}>Registrar</Button>
         </Modal.Footer>
       </Modal>
